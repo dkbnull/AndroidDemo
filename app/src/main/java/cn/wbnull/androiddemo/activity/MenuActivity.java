2732402a5b8a1fe8d7383a08e52fd2bdffa490a2
dkbnull <dkbnull@163.com>
2020-01-27 13:12:48 +0800

    Android中动态申请权限

--- app/src/main/java/cn/wbnull/androiddemo/activity/MenuActivity.java
+++ app/src/main/java/cn/wbnull/androiddemo/activity/MenuActivity.java
@@ -1,6 +1,16 @@
 package cn.wbnull.androiddemo.activity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import butterknife.OnClick;
 import cn.wbnull.androiddemo.R;
@@ -16,13 +26,85 @@ import cn.wbnull.androiddemo.tool.CommonTools;
 @ActivityLayoutInject(R.layout.activity_menu)
 public class MenuActivity extends BaseActivity {
 
+    private AlertDialog mDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestPermission();
     }
 
     @OnClick(R.id.menuBtnUpdate)
     public void onClickCheckUpdate() {
         CommonTools.startNewActivity(UpdateActivity.class);
+    }
+
+    public void requestPermission() {
+        //权限检查
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //申请权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                //已授权
+                if (grantResults[i] == 0) {
+                    continue;
+                }
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {
+                    //选择禁止
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("授权");
+                    builder.setMessage("需要允许授权才可使用");
+                    builder.setPositiveButton("去允许", (dialog, id) -> {
+                        if (mDialog != null && mDialog.isShowing()) {
+                            mDialog.dismiss();
+                        }
+                        ActivityCompat.requestPermissions(MenuActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    });
+                    mDialog = builder.create();
+                    mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.show();
+                } else {
+                    //选择禁止并勾选禁止后不再询问
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("授权");
+                    builder.setMessage("需要允许授权才可使用");
+                    builder.setPositiveButton("去授权", (dialog, id) -> {
+                        if (mDialog != null && mDialog.isShowing()) {
+                            mDialog.dismiss();
+                        }
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        //调起应用设置页面
+                        startActivityForResult(intent, 2);
+                    });
+                    mDialog = builder.create();
+                    mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            //应用设置页面返回后再次判断权限
+            requestPermission();
+        }
     }
 }
